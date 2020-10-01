e778ad4f5d3b3026ecb727761265927a59db2500
live <live@livevbox.example.org>
2020-10-01 12:59:55 -0500

    Ejemplo de debuging para un programa con segfaul y errores de logica

--- Clase_10/segfault.cpp
+++ Clase_10/segfault.cpp
@@ -10,16 +10,17 @@ int main(int argc, char **argv)
   array = new double [N];
   // initialise array
   for (int ii = 0; ii < N; ++ii) {
-    array[ii] = 2*(++ii); // != 2*(ii++) ? 
+    array[ii] = 2*(++ii); // != 2*(ii++) ?
   }
-  // print some values 
+  // print some values
   int idx = 2;
   std::cout << array[idx] << std::endl;
   idx = 10;
-  //std::cout << array[idx] << std::endl; // compiles, but ... 
+  //std::cout << array[idx] << std::endl; // compiles, but ...
 
   // free memory
   delete [] array;
+  //std::cout<<array[100]<<std::endl;
 
-  return EXIT_SUCCESS;
+  return 0;
 }
